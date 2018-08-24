8cfa5d22501c89d34c21b13b69aadd63f7989aaa
root <root@localhost.localdomain>
2018-08-24 00:34:41 +0000

    Small chunks of a very big map

---
+++ NetMap/curious.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for i in {1..254..1};do host $i'.'$i'.'$i'.'$i;sleep 1; done
+#EOF 
---
+++ NetMap/maps.py
@@ -0,0 +1,95 @@
+import pygeoip, sys, os, time
+import PIL.Image as image
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Map:
+    modes = {0:'US',1:'global'}
+    MODE = -1
+    # gi = pygeoip.GeoIP('GeoIP.dat')
+    ''' Example:
+    gi.country_code_by_name('google.com')
+    gi.country_code_by_addr('64.233.161.99')
+    gi.country_name_by_addr('64.233.161.99')
+    gi = pygeoip.GeoIP('GeoIPRegion.dat')
+    gi.region_by_name('apple.com')
+    gi = pygeoip.GeoIP('GeoIPCity.dat')
+    gi.record_by_addr('64.233.161.99')
+    gi = pygeoip.GeoIP('GeoIPISP.dat')
+    gi.isp_by_name('cnn.com')
+    '''
+
+    country_info = {}
+    city_info = {}
+    ip_info = {}
+
+    def __init__(self, mode):
+        self.MODE = self.modes[mode]
+        if self.MODE == 'US':
+            self.MAP = plt.imread('us_continental.jpg')
+            self.labeled = plt.imread('us_labeled.jpg')
+            self.showMap()
+
+    def getIPinfo(self):
+        os.system('./curious.sh >> ipdata.txt')
+        ipdat = open('ipdata.txt','r')
+        rawinfo = list()
+        for line in ipdat.readlines():
+            rawinfo.append(line)
+        print(str(len(rawinfo))+" hosts scanned")
+
+        return rawinfo
+
+    def showMap(self):
+        fig, axes = plt.subplots()
+        plt.imshow(self.MAP)
+        plt.show()
+        plt.imshow(self.labeled)
+        plt.show()
+        # Get the image data of the plain map
+        img = image.open('us_continental.jpg')
+        imgmatrix = np.array(img)
+        print(imgmatrix.shape)
+        # Get the image data of the labeled map
+        ref = image.open('us_labeled.jpg')
+        refmat = np.array(ref)
+        print(refmat.shape)
+
+
+def get_http(host, decoding='utf-8'):
+    """ Make HTTP GET request to host"""
+    return urlopen(host).read().decode(decoding)
+
+
+def geo_locate(host):
+    """ Get the GeoLocation of a remote host.
+    This resource was found from the Team Ultimate tool ReconDog,
+    which utilizes web requests to fetch GeoIP data.
+    """
+    geo = "http://ipinfo.io/" + host + "/geo"
+
+    try:
+        geodat = get_http(geo)
+        print(geodat)
+    except:
+        print('Bad IP Address! ')
+
+
+def main():
+    print('Making sure the Image libraries are installed')
+    os.system('pip install scipy')
+    os.system('pip install matplotlib')
+    print("Building a map")
+    if len(sys.argv) == 2:
+        print(sys.argv[1])
+        geo_locate(sys.argv[1])
+    else:
+        start = time.time()
+        iMap = Map(0)
+        iMap.getIPinfo()
+        print(str(time.time() - start)+"seconds left")
+
+
+if __name__ == '__main__':
+    main()
